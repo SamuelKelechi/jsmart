5be6d7c95a6ca0042ac55b6d7d01384190c90cc2
SamuelKelechi <kcfreshie@ymail.com>
2025-01-12 22:03:28 +0100

    Added More Components

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import './Services.css'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='Service_Container'>
+      <h2>COME AND ENJOY</h2>
+      <div className='Service_Container_Wrapper'>
+        <div className='Service_Contain1'>
+          <div className='Service_Contain1_Hold'>
+            <h2 className='Service_Header'>NIGHTLIFE</h2>
+            <span className='Service_Text'>Experience Adorned with Opulent Décor.</span>
+          </div>
+        </div>
+        <div className='Service_Contain2'>
+          <div className='Service_Contain1_Hold'>
+            <h2 className='Service_Header'>DRINKS AND LOUNGE</h2>
+            <span className='Service_Text'>Elevating Your Spirit</span>
+          </div>
+        </div>
+        <div className='Service_Contain3'>
+          <div className='Service_Contain1_Hold'>
+            <h2 className='Service_Header'>DELICIOUS CUISINE</h2>
+            <span className='Service_Text'>Food & Savory Delights</span>
+          </div>
+        </div>
+      </div>
+      <a
+        href="https://wa.me/234"
+        className="order-button"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Order Now
+      </a>
+    </div>
   )
 }
 
---
+++ src/components/Home/Weekly/Weekly.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Weekly = () => {
+  return (
+    <div>Checkout Our Weekly Activities <br/> Loading...</div>
+  )
+}
+
+export default Weekly
