22f7c31c31aada86e4df70850e8def236031deaf
SamuelKelechi <kcfreshie@ymail.com>
2025-01-25 19:00:30 +0100

    Added About Us Page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,6 +10,9 @@ const About = () => {
          <div className='About_Hero'>
             <h1 className='About_Header'>ABOUT US</h1>
         </div>
+        <div className='About_Content'>
+          loading...
+        </div>
     </div>
   )
 }
